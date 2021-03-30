e343272856f18d12c2427e0b1abe577a21770303
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-03-29 19:53:07 -0500

    primera entrega

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+// class RegularClass {}
+// class ComponentClass extends Component {}
+
+// const regularClassInstance = new RegularClass ();
+// const componentClassIntance = new ComponentClass ();
+
+// console.log ('regularClassInstance', regularClassInstance); //se agrega un resgisto regular que ene ste caso es le que esta en '' y despues el objeto real en si
+// console.log ('componentClassIntance', componentClassIntance);
+
+class App extends Component { //creamos una clase llamada App la cual sera llamada mas delante como un componente. 
+    //linea 24: yo vivo en colombia, y codifico todos los dias.
+    //linea 25: my lenguaje favorito es javascript y creo que react.js, es asombroso.
+    //linea 26: ademas de codificar, tambian amo la musica, el dibujo y las artes.
+    //linea 34: se exporta a aca la variable para que el texto dentro de bio aparezca aca.
+    constructor() {
+        super(); //se llama super para invocar al constructor de la clase de componente padre, si no se pone, genera error por que el this.state no se ha definido
+        this.state = {displayBio: false};//displayBio sera un valoir que controla si debemos mostrar  o no esta nueva seccion.
+    }
+
+    readMore() {
+        this.setState({displayBio = true})
+    }
+
+    render() {
+            //ESTO Ya no se necesita porque el if se puso arriba con el signo (?)
+        // if (!this.state.displayBio) { // la admiracion (!) convierte a false todo el contexto adentro,
+        //     bio = null; //cuando react ve un valor nulo dentro de jsx decide omitir eso  yno genera nada
+
+        // }
+        return (
+            <div>
+                <h1>Hello</h1>
+                <p>My name is Jonatan. I am developer. </p>
+                <p>I 'm always looking forward to working on meaningful proyects.</p>
+                {
+                    this.state.displayBio ?(
+                        <div>
+                            <p>I live in Colombia, and code  every day</p> 
+                            <p>My favorite language is JavaScript, and i think Reack.js is awesome</p>
+                            <p>Besides coding, i also love music, the drew and arts. </p>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick={this.readMore}>Read more</button>
+                        </div>
+                    )
+                } 
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root')); //<App/> es un etiqueta creada desde la clase que creams en App.js. se puede usar asi tambien <App></App>
+
+// class Animal {
+//     constructor (name, age) {
+//         this.name = name ;
+//         this.age = age;
+//     }
+
+//     speak() {
+//         console.log ('I am', this.name, 'and i am ', this.age, 'years olg');
+//     }
+// }
+
+// const animal1 = new Animal('simba', 3);
+// animal1.speak(); //aqui estoy llamando a la constante animal1 y a la clase speak
+
+// console.log (animal1) //aqui estoy llamando por consola a animal1
+
+// class Lion extends Animal {
+//     constructor (name, age, furColor, speed, puppies ) { //esta clase debe de conservar los valores de la clase pricipal como name y age, fucolor y speed ya son propiedades de esta clase
+//         super (name, age); //esto se hace para llamar a las propiedades de la clase animal
+//         this.furColor= furColor;
+//         this.speed= speed;
+//         this.puppies= puppies;
+
+//     }
+
+//     roar() {
+//         console.log (
+//             'ROOAR! I Have', 
+//             this.furColor, 
+//             'fur, and i can run', 
+//             this.speed, 
+//             'miles an hour!, and i have',
+//             this.puppies,
+//             'puppies'
+//         );
+//     }
+// }
+
+// const lion1 =new Lion('Mufasa', 20, 'golden', 25, 5);
+// lion1.speak();
+// lion1.roar();
+// console.log (lion1);
+
+
+
+// const lion2 =new Lion ('king', 30, 'red', 40);
+// lion2.speak();
+// lion2.roar ();
+// console.log (lion2);
