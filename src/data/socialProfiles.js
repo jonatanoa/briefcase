4bff7927faedae3f2b73bc84159b8a5a8f382283
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-02 15:31:56 -0500

    quinta entrega

--- src/data/socialProfiles.js
+++ src/data/socialProfiles.js
@@ -9,23 +9,23 @@ const SOCIAL_PROFILES = [
         id: 1,
         link: 'mailto:jonathan950904@gmail.com',
         image: emailIcon
-    }
+    },
 
     {
         id: 2,
         link: 'https://github.com/jonatanparamosoftware',
         image: githubIcon
-    }
+    },
     {
         id: 3,
         link: 'https://www.linkedin.com/in/jonatan-osorio-arcila-618818138/',
         image: linkedinIcon
-    }
+    },
     {
         id: 4,
         link: 'https://www.fiverr.com/jonatanosorioa?public_mode=true',
         image: fiverrIcon
-    }
+    },
     {
         id: 5,
         link: 'https://www.upwork.com/freelancers/~0102314e5686bc66b2',
