bbd15c1cb4ffa782e6f291fa7dfa03de37fd8f36
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-01 20:10:01 -0500

    cuarta entrega

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,38 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import fiverrIcon from '../assets/fiverr.png';
+import upworkIcon from '../assets/upwork.png';
+
+const SOCIAL_PROFILES = [
+    {
+        id: 1,
+        link: 'mailto:jonathan950904@gmail.com',
+        image: emailIcon
+    }
+
+    {
+        id: 2,
+        link: 'https://github.com/jonatanparamosoftware',
+        image: githubIcon
+    }
+    {
+        id: 3,
+        link: 'https://www.linkedin.com/in/jonatan-osorio-arcila-618818138/',
+        image: linkedinIcon
+    }
+    {
+        id: 4,
+        link: 'https://www.fiverr.com/jonatanosorioa?public_mode=true',
+        image: fiverrIcon
+    }
+    {
+        id: 5,
+        link: 'https://www.upwork.com/freelancers/~0102314e5686bc66b2',
+        image: upworkIcon
+    }
+   
+
+];
+
+export default SOCIAL_PROFILES;
