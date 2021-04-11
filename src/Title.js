b23482f719a0504ddc3769a123c5ee89f5fe43d7
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-10 19:32:02 -0500

    septima entrega

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const TITLES = [
+    'a sofware engineer',
+    'a music love',
+    'an enthusiastic learner',
+    'an adventure seeker'
+]
+
+class Title extends Component {
+    
+}
