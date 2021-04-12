bfa4559c9b6813828efd0bc3ecd483bfa65591e7
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-12 13:45:32 -0500

    octava entrega

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,9 @@
-import React, { Component} from 'react';
+import React from 'react';
 import PROJECTS from '../data/projects';
 
-class Project extends Component {
-    render() {
-        console.log('this.props', this.props);
+const Project = props => {
 
-        const { title, image, description, link } = this.props.project; 
+        const { title, image, description, link }= props.project; 
 
         return (
             <div style={{display: 'inline-block', width: 300, margin: 30}}>
@@ -15,32 +13,25 @@ class Project extends Component {
                 <a href={link}>{link}</a>
             </div>
         )
-    }
 }
 
-class Projects extends Component {
-
-    render () {
-        return (
-            <div>
-                <h2>Highlighted Projects</h2>
-                <div>
-                    {
+const Projects =()=> (
+    <div>
+        <h2>Highlighted Projects</h2>
+        <div>
+             {
                     
-                        PROJECTS.map(PROJECT=> {//esto es el equivalente a lo que tengo abajo.
-                            return (//Cuando un elemento de React, muestra más de un mismo tipo de elemento, debe poder identificar qué elementos es cual, para poder actualizarlo correctamente. El Key debe de ser un valor único en la colección y se utiliza por lo general el ID o un campo de los ítems que sea único
-                                <Project key={PROJECT.id} project={PROJECT}/> //PROJECTS.title, es para que en el navegador se muestren los titulos. 
-                            );
-                        })
+            PROJECTS.map(PROJECT=> (//esto es el equivalente a lo que tengo abajo.
+                 //Cuando un elemento de React, muestra más de un mismo tipo de elemento, debe poder identificar qué elementos es cual, para poder actualizarlo correctamente. El Key debe de ser un valor único en la colección y se utiliza por lo general el ID o un campo de los ítems que sea único
+                <Project key={PROJECT.id} project={PROJECT}/> //PROJECTS.title, es para que en el navegador se muestren los titulos. 
+                 ))
                     
-                    /* <div>{PROJECTS [0]. title}</div>
-                    <div>{PROJECTS [1]. title}</div>
-                    <div>{PROJECTS [2]. title}</div> *///long way} 
-                    }
-                </div>
-            </div>
-        )
-    }
-} 
+             /* <div>{PROJECTS [0]. title}</div>
+             <div>{PROJECTS [1]. title}</div>
+             <div>{PROJECTS [2]. title}</div> *///long way} 
+            }
+        </div>
+    </div>
+        ) 
 
 export default  Projects;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './index.css';
 
 ReactDOM.render(<App/>, document.getElementById('root')); //<App/> es un etiqueta creada desde la clase que creams en App.js. se puede usar asi tambien <App></App>
 
