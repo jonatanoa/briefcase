22230f5b4e7e2f1b1a91ebf98e72b5777534935c
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-12 13:51:31 -0500

    novena entrega

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,16 +3,16 @@ import PROJECTS from '../data/projects';
 
 const Project = props => {
 
-        const { title, image, description, link }= props.project; 
+    const { title, image, description, link }= props.project; 
 
-        return (
-            <div style={{display: 'inline-block', width: 300, margin: 30}}>
-                <h3>{title}</h3>
-                <img src={image} alt='profile' style={{width: 200, height: 120}}/> 
-                <p>{description}</p>
-                <a href={link}>{link}</a>
-            </div>
-        )
+    return (
+        <div style={{display: 'inline-block', width: 300, margin: 30}}>
+            <h3>{title}</h3>
+            <img src={image} alt='profile' style={{width: 200, height: 120}}/> 
+            <p>{description}</p>
+            <a href={link}>{link}</a>
+        </div>
+    )
 }
 
 const Projects =()=> (
--- src/components/SocialProfiles.js
+++ src/components/SocialProfiles.js
@@ -2,34 +2,30 @@ import React, {Component}from 'react';
 import SOCIAL_PROFILES from '../data/socialProfiles';
 
 
-class SocialProfile extends Component{
-    render(){
-        const {link, image} = this.props.socialProfile;
-        return (
-            <span>
-                <a href={link} target="_new">
-                    <img src={image} alt='social-profile' style={{width: 35,margin: 10}}/>
-                </a>
-            </span>
-        )
-    }
+const SocialProfile = props =>{
+
+    const {link, image} = props.socialProfile;
+    return (
+        <span>
+            <a href={link} target="_new">
+                <img src={image} alt='social-profile' style={{width: 35,margin: 10}}/>
+            </a>
+        </span>
+    )
 }
 
-class SocialProfiles extends Component {
-    render () {
-        return (
-            <div>
-                <h2>Contact me</h2>
-                <div>
-                    {
-                        SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
-                            return <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE}/>;
-                        })
-                    }
-                </div>
-            </div>
-        )
-    }
-}
+const SocialProfiles =()=>  (
+    <div>
+        <h2>Contact me</h2>
+        <div>
+            {
+                SOCIAL_PROFILES.map(SOCIAL_PROFILE => {
+                    return <SocialProfile key={SOCIAL_PROFILE.id} socialProfile={SOCIAL_PROFILE}/>;
+                })
+            }
+        </div>
+    </div>
+)
+
 
 export default SocialProfiles;
