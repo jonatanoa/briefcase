faca51506935d9a8365b91d3d1ee027e79712fb0
jonatanparamosoftware <78571606+jonatanparamosoftware@users.noreply.github.com>
2021-04-12 19:48:23 -0500

    novena entrega

--- src/components/Jokes.js
+++ src/components/Jokes.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
 
+const Joke = ({joke}) => {
+    const {setup, punchline} =joke;
+
+    return <p>{setup} <em>{punchline}</em></p>
+}
+
 export default class Jokes extends Component {
 
-    state = { joke: {} }; //objeto vacio
+    state = { joke: {}, jokes:[] }; //objeto vacio
 
     componentDidMount () {
         fetch ('https://official-joke-api.appspot.com/random_joke')
@@ -10,13 +16,23 @@ export default class Jokes extends Component {
         .then(json => this.setState({joke: json}));
     }
 
+    fetchJokes = () => {
+        fetch ('https://official-joke-api.appspot.com/random_ten')
+        .then (response=> response.json())
+        .then(json => this.setState({jokes: json}));
+    } 
+
     render() {
 
         const {setup, punchline} = this.state.joke;
         return (
             <div>
                 <h2>highligthted joke</h2>
-                <p>{setup} <em>{punchline}</em></p>
+                <Joke joke ={this.state.joke}/>
+                <hr/>
+                <h3>Want ten new jokes? </h3>
+                <button onClick={this.fetchJokes}>Click me!</button>
+                {this.state.jokes.map (joke =>  (<joke key={joke.id} joke={joke}/>))}
             </div>
         )
     }
